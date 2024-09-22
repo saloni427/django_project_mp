40bd611303d757b151e6099101f95337c282e530
Saloni <deshmukhsaloni427@gmail.com>
2024-09-22 21:25:40 +0530

    updated

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-epjfqr*+nvr%#v#v&@wo0ow^l!8zb!@!s^$8$p35(8b(7vedgc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
